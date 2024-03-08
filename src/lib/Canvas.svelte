526872f74c5d447d3c88cdf6d702248b42cbdfb7
sakelig <sak.sakelig@gmail.com>
2024-03-08 23:09:04 +0100

    setup

---
+++ src/lib/Canvas.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let width = 600;
+	export let height = 500;
+	export let color = '#333';
+	export let background = '#AAAAAA';
+
+	let canvas: HTMLCanvasElement;
+	let context: CanvasRenderingContext2D | null;
+
+	onMount(() => {
+		context = canvas.getContext('2d');
+	});
+
+	const peepoImage: HTMLImageElement = new Image();
+	peepoImage.onload = () => {
+		context?.drawImage(peepoImage, 0, 0);
+	};
+
+	peepoImage.src = 'https://cdn.discordapp.com/emojis/925076402573824011.webp';
+</script>
+
+<canvas {width} {height} style:background bind:this={canvas} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Canvas from '$lib/Canvas.svelte';
+</script>
+
+<div style="display: flex; justify-content: center;">asdfasdf</div>
+<div style="display: flex; align-items: center; justify-content: center;">
+	<Canvas />
+</div>
